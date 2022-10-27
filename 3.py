6a4f4558d7a2d13a0b452b0d1dfd87b65e748285
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-10-27 18:56:15 +0300

    task 3 finished

---
+++ 3.py
@@ -0,0 +1,34 @@
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+
+def encode(input_string):
+
+    count = 1
+    prev = ''
+    lst = []
+    for character in input_string:
+        if character != prev:
+            if prev:
+                entry = (prev,count)
+                lst.append(entry)
+            count = 1
+            prev = character
+        else:
+            count += 1
+    else:
+        entry = (character,count)
+        lst.append(entry)
+    return ''.join(chr(cnt)+ch for (ch, cnt) in lst)
+
+def decode(encoded_text):
+    """ Simple run-length decoder. """
+    chars = []
+    a = iter(encoded_text)
+    for (cnt, ch) in zip(a, a):
+        chars.append(ch * ord(cnt))
+    return ''.join(chars)
+
+word = "fffaaarry"
+rle_text = encode(word)
+
+print(decode(rle_text))
